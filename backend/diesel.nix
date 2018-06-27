5fe40be2ad2d3fbff6fca93015150ca2b0114d4c
Greg Hale <imalsogreg@gmail.com>
2018-06-27 09:28:35 -0400

    add nix packaging

---
+++ backend/diesel.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "rust";
+    buildInputs = [
+        postgresql
+        rust.cargo
+        rust.rustc
+    ];
+    shellHook = ''
+        export OPENSSL_DIR="${openssl.dev}"
+        export OPENSSL_LIB_DIR="${openssl.out}/lib"
+        export PATH="$PWD/bin:$PATH"
+        export DATABASE_URL="postgres://postgres@localhost/db"
+        if ! type diesel > /dev/null 2> /dev/null; then
+          cargo install diesel_cli --no-default-features --features postgres --root $PWD
+        fi
+        diesel setup
+    '';
+}
---
+++ backend/shell.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "rust-env";
+  buildInputs = [
+    rustc cargo
+
+    # Example Additional Dependencies
+    pkgconfig openssl
+  ];
+
+  # Set Environment Variables
+  RUST_BACKTRACE = 1;
+}
